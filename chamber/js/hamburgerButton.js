9a275feddc60e390815b320578893ae43e4488fd
ctfuller98 <ful17005@byui.edu>
2022-05-14 11:02:41 -0600

    grid changes and nav button changes

--- chamber/js/hamburgerButton.js
+++ chamber/js/hamburgerButton.js
@@ -1,6 +1,7 @@
-// Hamburger button
-const hamburgerButton = document.querySelector('.hambugerBtn');
-const miniNav = document.querySelector('.navigation');
+const hambutton = document.querySelector('.ham');
+const mainnav = document.querySelector('.navigation')
 
-hamburgerButton.addEventListener('click', () => {miniNav.classList.toggle('responsive')}, false);
-hamburgerButton.addEventListener('click', () => {hamburgerButton.classList.toggle('responsive')}, false);
+hambutton.addEventListener('click', () => {mainnav.classList.toggle('responsive')}, false);
+
+// To solve the mid resizing issue with responsive class on
+window.onresize = () => {if (window.innerWidth > 760) mainnav.classList.remove('responsive')};
